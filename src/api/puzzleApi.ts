2588ec38583781cc078c2104b7d25a1e1a81f4af
jonnyell89 <jonnyell89@gmail.com>
2026-09-09 10:10:17 +0100

    makeGuess function added to puzzleApi module.

--- src/api/puzzleApi.ts
+++ src/api/puzzleApi.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
 import type { PuzzleResponse } from "../interfaces/PuzzleResponse";
+import type { GuessResponse } from "../interfaces/GuessResponse";
+import type { GuessRequest } from "../interfaces/GuessRequest";
 
 const BASE_URL = "http://localhost:8080/api/puzzles"
 
 export async function createPuzzle(difficulty: string): Promise<PuzzleResponse> {
     const response = await axios.post<PuzzleResponse>(
         `${BASE_URL}?difficulty=${difficulty}`
+    );
+    return response.data;
+}
+
+export async function makeGuess(id: number, guessRequest: GuessRequest): Promise<GuessResponse> {
+    const response = await axios.post<GuessResponse>(
+        `${BASE_URL}/${id}/guesses`,
+        guessRequest
     );
     return response.data;
 }
---
+++ src/interfaces/GuessRequest.ts
@@ -0,0 +1,5 @@
+export interface GuessRequest {
+    row: number;
+    col: number;
+    value: number;
+}
